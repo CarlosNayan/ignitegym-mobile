bb31d5735882ebd8364aa20ba3177104220b136b
Carlos Nayan <carlosnayan@gmail.com>
2024-07-19 19:34:37 -0300

    added theme provider on application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
+import { StatusBar, Text, View } from "react-native";
+import { Loading } from "src/components/Loading";
+import { THEME } from "src/theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <View>
+        <StatusBar translucent barStyle="light-content" />
+        {!fontsLoaded ? (
+          <Text style={{ fontFamily: "Roboto_700Bold" }}>
+            Open up App.tsx to start working on your app!
+          </Text>
+        ) : (
+          <Loading />
+        )}
+      </View>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
